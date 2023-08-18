73d4f0be22a2620f809a7c3e1979c25ba674e11e
TeamBocean <112326837+TeamBocean@users.noreply.github.com>
2023-08-18 17:00:51 +0000

    modal rewritten in jquery

--- js/script.js
+++ js/script.js
@@ -6,31 +6,51 @@ AOS.init({
     offset: 100,
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Get the modal
-    var modal = document.getElementById("myModal");
+document.addEventListener('DOMContentLoaded', function () {
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("showModal");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close-btn")[0];
 
     // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        event.preventDefault();
-        modal.style.display = "flex";
-    }
+
+    var modal = $("#myModal");
+    $("#showModal").on("click", function (event) {
+        modal.css("display", "flex");
+    });
+
+    var modalTwo = $(".modalTwo");
+    $(".showModalTwo").on("click", function (event) {
+        modalTwo.css("display", "flex");
+    });
+    var modalThree = $(".modalThree");
+    $("#showModalThree").on("click", function (event) {
+        console.log(event.target)
+        modalThree.css("display", "flex");
+    });
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+    $(".close-btn").on("click", function (event) {
+        if (event.target.classList.contains('close-btn')) {
+            modal.css("display", "none");
+        }
+    });
+    $(".close-btnTwo").on("click", function (event) {
+        if (event.target.classList.contains('close-btnTwo')) {
+            modalTwo.css("display", "none");
+        }
+    });
+    $(".close-btnThree").on("click", function (event) {
+        if (event.target.classList.contains('close-btnThree')) {
+            modalThree.css("display", "none");
+        }
+    });
 
     // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+    window.onclick = function (event) {
+        if (event.target.classList.contains('modal')) {
+            modal.css("display", "none");
+        } else if (event.target.classList.contains('modalTwo')) {
+            modalTwo.css("display", "none")
+        } else if (event.target.classList.contains('modalThree')) {
+            modalThree.css("display", "none")
         }
     }
 });
